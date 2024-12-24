abff382d96b1e4ed1a9b61a3f57ad710de791809
miguelamoreira <40210256@esmad.ipp.pt>
2024-12-24 00:10:58 +0000

    missing semicolon in Badge model

--- models/Badge.model.js
+++ models/Badge.model.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, DataTypes) => {
     const Badge = sequelize.define("Badge", {
         badge_id: { 
             type: DataTypes.INTEGER, 
-            primaryKey: true 
+            primaryKey: true, 
             autoIncrement: true, 
         },
         name: { 
